1d168244730f547129a5028d1a4f49cecad52fdf
ohnotomsutton <146578846+ohnotomsutton@users.noreply.github.com>
2026-03-07 17:40:04 -0700

    Update sw.js

--- nopepad/sw.js
+++ nopepad/sw.js
@@ -1,16 +1,30 @@
-const CACHE = 'nopepad-v1';
+const CACHE = 'nopepad-v2';
 const FILES = [
   '/nopepad.html',
   '/manifest.json',
   '/icon-192.png',
-  '/icon-512.png'
+  '/icon-512.png',
+  'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500&family=JetBrains+Mono:wght@400;500&family=Fira+Code:wght@400;500&family=Inconsolata:wght@400;500&family=Source+Code+Pro:wght@400;500&family=Courier+Prime:wght@400;700&display=swap'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+});
 
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (e.request.url.includes('fonts.gstatic.com')) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }))
+  );
 });
