26d9f45a383a9063f6fb03729d6431bc569d7420
ohnotomsutton <146578846+ohnotomsutton@users.noreply.github.com>
2026-03-07 17:47:05 -0700

    Update sw.js

--- nopepad/sw.js
+++ nopepad/sw.js
@@ -1,10 +1,9 @@
-const CACHE = 'nopepad-v2';
+const CACHE = 'nopepad-v1';
 const FILES = [
   '/nopepad.html',
   '/manifest.json',
   '/icon-192.png',
   '/icon-512.png',
-  'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500&family=JetBrains+Mono:wght@400;500&family=Fira+Code:wght@400;500&family=Inconsolata:wght@400;500&family=Source+Code+Pro:wght@400;500&family=Courier+Prime:wght@400;700&display=swap'
 ];
 
 self.addEventListener('install', e => {
