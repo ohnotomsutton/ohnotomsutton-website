8a92c075b4b3ecece742b633bd586974f0e34d5e
ohnotomsutton <146578846+ohnotomsutton@users.noreply.github.com>
2026-03-07 17:57:33 -0700

    Add files via upload

---
+++ nopepad/sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'nopepad-v1';
+const FILES = [
+  '/nopepad/nopepad.html',
+  '/nopepad/manifest.json',
+  '/nopepad/icon-192.png',
+  '/nopepad/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (e.request.url.includes('fonts.gstatic.com')) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }))
+  );
+});
